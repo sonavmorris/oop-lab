a23e29e2f7504fedb4c655518e17304f607b6a1f
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-03-20 15:45:11 +0530

    Add files via upload

--- student.java
+++ student.java
@@ -1,21 +1,17 @@
-package sona;
-import java.util.Scanner;
+package student;
+import java.util.*;
 public class student {
-	public static void main(String[] args){
-		Scanner sc= new Scanner(System.in);
-		String name=sc.nextLine();
-		char gender=sc.next().charAt(0);
-		int age=sc.nextInt();
-		long mobileno=sc.nextLong();
-		double cgpa=sc.nextDouble();
-		System.out.println("Name:"+name);
-		System.out.println("Gender:"+gender);
-		System.out.println("Age:"+age);
-		System.out.println("mobileNo:"+mobileno);
-		System.out.println("CGPA:"+cgpa);
-		sc.close();
-
-	
-
-}
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter the rollno: ");
+        int rollno = sc.nextInt();
+        System.out.println("Enter the mark: ");
+        int mark = sc.nextInt();
+        System.out.println("Name: "+name);
+        System.out.println("Roll No: "+rollno);
+        System.out.println("Mark: "+mark);
+        sc.close();
+    }
 }
