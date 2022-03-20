5c94d9386f069d64d7ac0baf4b343645bb25a14b
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-03-20 15:49:30 +0530

    Add files via upload

---
+++ stud.java
@@ -0,0 +1,23 @@
+package student;
+import java.util.Scanner;
+public class stud {
+		public static void main(String[] args){
+			Scanner sc= new Scanner(System.in);
+			String name=sc.nextLine();
+			char gender=sc.next().charAt(0);
+			int age=sc.nextInt();
+			long mobileno=sc.nextLong();
+			double cgpa=sc.nextDouble();
+			System.out.println("Name:"+name);
+			System.out.println("Gender:"+gender);
+			System.out.println("Age:"+age);
+			System.out.println("mobileNo:"+mobileno);
+			System.out.println("CGPA:"+cgpa);
+			sc.close();
+
+
+
+	}
+	}
+
+
